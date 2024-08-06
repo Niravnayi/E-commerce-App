29783f9ace34a1289bb2351964b2ae1c4db9935a
Nirav <niravlimbachia031@gmail.com>
2024-08-06 10:56:01 +0530

    Set up

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  entry: "./src",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "yagnesh modh",
+      template: "./public/index.html",
+      filename: "index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  output: {
+    path: path.resolve(__dirname, "output"),
+    filename: "main.js",
+    publicPath: "/",
+  },
+  mode: "development",
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+    alias: {
+      src: path.resolve(__dirname, "src"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+};
